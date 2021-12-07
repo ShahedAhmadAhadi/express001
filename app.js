c0c9938577fac64d60c3807cc8aef4f3fa24d659
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-07 12:25:29 +0430

    products route developed and work will with queries passed to url

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { products } = require('./data')
 
 app.use(express.static('./public'))
 
 app.get('/api', (req, res) => {
-    res.json({result: 'success'})
+    res.send('<h1>Home Page</h1> <a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const {search, limit} = req.query
+    let queryProducts = products
+    if (search) {
+        if (search) {
+            queryProducts = products.filter((product) => {
+                return product.name.startsWith(search)
+            })
+        }
+    }
+
+    if (limit) {
+        if (search) {
+            queryProducts = queryProducts.slice(0, +limit)
+        }
+    }
+
+    if (search && queryProducts.length < 1 && +limit !== 0) {
+        return res.send('<h1>No product match with such query</h1>')
+    }
+
+    const newProducts = queryProducts.map((product) => {
+        const { id, name, image } = product
+        return {id, name, image}
+    })
+    res.json(newProducts )
+})
+
+app.get(`/api/products/:productId`, (req, res) => {
+    const { productId } = req.params
+    const product = products.find(item => item.id === +productId)
+    if (!product) {
+        return res.status(404).send('Product does not exitst')
+    }
+    console.log(product)
+    res.json(product)
+})
+
+app.get('/api/query', (req, res) => {
+    const {query} = req
+    res.json({'req': 'Request for query params', data: query})
 })
 
 app.get('/', (req, res) => {
