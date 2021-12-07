662162cb2c54b21bb96d0661e7affe466cf73e4c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-07 11:10:03 +0430

    get requests and 404 req resolved

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).send('Home page')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resourse not Found, 404</h1>')
+})
+
 app.listen(5000, () => {
     console.log('server listening')
 })
